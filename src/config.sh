ed92d316c6c581a20c156f7dc9ea6e1f14b7bcae
iadfeshchm <zlatov@ya.ru>
2016-10-25 12:14:25 +0300

    q

--- src/config.sh
+++ src/config.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-DBHOST="$DB_HOST"
-DBUSER="$DB_USER"
-DBPASS="$DB_PASS"
-DBNAME="$DB_NAME"
-
-echo "База данных: $DB_NAME"
-echo "База данных: $DBNAME"
-
 TEXT_BOLD='\033[1m'
 COLOR_RED='\033[31m'
 COLOR_GREEN='\033[32m'
