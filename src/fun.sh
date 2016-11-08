58980592e58191c8e63832a4931d4892321b79b3
iadfeshchm <zlatov@ya.ru>
2016-11-08 10:20:11 +0300

    Триггеры если миграции без ошибок и версия больше 0, процедуры аналогично

--- src/fun.sh
+++ src/fun.sh
@@ -255,83 +255,99 @@ function migrateToLastVersion {
         VERSIONS=("${VERSIONS[@]}" "$line")
     done < <(LANG=C ls migration | grep  '.sql' | sed -r 's/\.sql//')
 
-    # Перебор и выполнение необходимых миграций
-    dbArray=(${DB_VERSION//./ })
-    for mVersion in "${VERSIONS[@]}"
-    do
-        mArray=(${mVersion//./ })
-        if [[ ${mArray[0]} -gt ${dbArray[0]} ]]
-            then
-                migrate $mVersion
-                if [ $MIGRATION_STATUS -eq 1 ]; then break; fi
-            else
-                if [[ ${mArray[1]} -gt ${dbArray[1]} ]]
+    if [[ ${#VERSIONS[*]} -gt 0 ]]
+        then
+            # Перебор и выполнение необходимых миграций
+            dbArray=(${DB_VERSION//./ })
+            for mVersion in "${VERSIONS[@]}"
+            do
+                mArray=(${mVersion//./ })
+                if [[ ${mArray[0]} -gt ${dbArray[0]} ]]
                     then
                         migrate $mVersion
                         if [ $MIGRATION_STATUS -eq 1 ]; then break; fi
                     else
-                        if [[ ${mArray[2]} -gt ${dbArray[2]} ]]
+                        if [[ ${mArray[1]} -gt ${dbArray[1]} ]]
                             then
                                 migrate $mVersion
                                 if [ $MIGRATION_STATUS -eq 1 ]; then break; fi
+                            else
+                                if [[ ${mArray[2]} -gt ${dbArray[2]} ]]
+                                    then
+                                        migrate $mVersion
+                                        if [ $MIGRATION_STATUS -eq 1 ]; then break; fi
+                                fi
                         fi
                 fi
-        fi
-    done
-
-    declare -a TRIGGERS
-    while read line
-    do
-        TRIGGERS=("${TRIGGERS[@]}" "$line")
-    done < <(LANG=C ls triggers | grep  '.sql' | sed -r 's/\.sql//')
-    if [[ ${#TRIGGERS[*]} -gt 0 ]]; then
-        for trigger in "${TRIGGERS[@]}"
-        do
-            echo -en $COLOR_GREEN
-            echo -e "Применение триггеров в $STYLE_DEFAULT$trigger:"
-            echo -en $STYLE_DEFAULT
-            (mysql --host=$DBHOST --port=3306 --user="$DBUSER" --database="$DBNAME" -s < "./triggers/$trigger") >/dev/null
-            TRIGGER_STATUS=$?
-            if [ $TRIGGER_STATUS -eq 0 ]
-                then
-                    echo -en $COLOR_GREEN
-                    echo "Успешно."
-                    echo -en $STYLE_DEFAULT
-                else
-                    echo -en $COLOR_RED
-                    echo -e "Ошибка применения триггеров."
-                    echo -en $STYLE_DEFAULT
-                    break
-            fi
-        done
+            done
+        else
+            MIGRATION_STATUS=0
     fi
 
-    declare -a PROCEDURES
-    while read line
-    do
-        PROCEDURES=("${PROCEDURES[@]}" "$line")
-    done < <(LANG=C ls procedures | grep  '.sql' | sed -r 's/\.sql//')
-    if [[ ${#PROCEDURES[*]} -gt 0 ]]; then
-        for procedure in "${PROCEDURES[@]}"
-        do
-            echo -en $COLOR_GREEN
-            echo -e "Применение процедур в $STYLE_DEFAULT$procedure:"
-            echo -en $STYLE_DEFAULT
-            (mysql --host=$DBHOST --port=3306 --user="$DBUSER" --database="$DBNAME" -s < "./procedures/$procedure") >/dev/null
-            PROCEDURE_STATUS=$?
-            if [ $PROCEDURE_STATUS -eq 0 ]
+    dbVersionArray=(${DB_VERSION//./ })
+    if [ $MIGRATION_STATUS -eq 0 ] && [ ${dbVersionArray[0]} -gt 0 ]
+        then
+            declare -a TRIGGERS
+            while read line
+            do
+                TRIGGERS=("${TRIGGERS[@]}" "$line")
+            done < <(LANG=C ls triggers | grep  '.sql' | sed -r 's/\.sql//')
+            if [[ ${#TRIGGERS[*]} -gt 0 ]]
                 then
-                    echo -en $COLOR_GREEN
-                    echo "Успешно."
-                    echo -en $STYLE_DEFAULT
+                    for trigger in "${TRIGGERS[@]}"
+                    do
+                        echo -en $COLOR_GREEN
+                        echo -e "Применение триггеров в $STYLE_DEFAULT$trigger:"
+                        echo -en $STYLE_DEFAULT
+                        (mysql --host=$DBHOST --port=3306 --user="$DBUSER" --database="$DBNAME" -s < "./triggers/$trigger") >/dev/null
+                        TRIGGER_STATUS=$?
+                        if [ $TRIGGER_STATUS -eq 0 ]
+                            then
+                                echo -en $COLOR_GREEN
+                                echo "Успешно."
+                                echo -en $STYLE_DEFAULT
+                            else
+                                echo -en $COLOR_RED
+                                echo -e "Ошибка применения триггеров."
+                                echo -en $STYLE_DEFAULT
+                                break
+                        fi
+                    done
                 else
-                    echo -en $COLOR_RED
-                    echo -e "Ошибка применения процедур."
-                    echo -en $STYLE_DEFAULT
-                    break
+                    TRIGGER_STATUS=0
             fi
-        done
     fi
+
+    if [ $MIGRATION_STATUS -eq 0 ] && [ $TRIGGER_STATUS -eq 0 ] && [ ${dbVersionArray[0]} -gt 0 ]
+        then
+            declare -a PROCEDURES
+            while read line
+            do
+                PROCEDURES=("${PROCEDURES[@]}" "$line")
+            done < <(LANG=C ls procedures | grep  '.sql' | sed -r 's/\.sql//')
+            if [[ ${#PROCEDURES[*]} -gt 0 ]]; then
+                for procedure in "${PROCEDURES[@]}"
+                do
+                    echo -en $COLOR_GREEN
+                    echo -e "Применение процедур в $STYLE_DEFAULT$procedure:"
+                    echo -en $STYLE_DEFAULT
+                    (mysql --host=$DBHOST --port=3306 --user="$DBUSER" --database="$DBNAME" -s < "./procedures/$procedure") >/dev/null
+                    PROCEDURE_STATUS=$?
+                    if [ $PROCEDURE_STATUS -eq 0 ]
+                        then
+                            echo -en $COLOR_GREEN
+                            echo "Успешно."
+                            echo -en $STYLE_DEFAULT
+                        else
+                            echo -en $COLOR_RED
+                            echo -e "Ошибка применения процедур."
+                            echo -en $STYLE_DEFAULT
+                            break
+                    fi
+                done
+            fi
+    fi
+
 }
 
 function migrate {
